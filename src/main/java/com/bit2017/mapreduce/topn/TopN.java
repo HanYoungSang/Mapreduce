37c4c2c8ef737a34cc44f10badc1db08148b892a
HAN <hanys123@gmail.com>
2017-03-22 15:52:44 +0900

    삽입 알고리즘 수정

--- src/main/java/com/bit2017/mapreduce/topn/TopN.java
+++ src/main/java/com/bit2017/mapreduce/topn/TopN.java
@@ -38,7 +38,7 @@ public class TopN {
 			newItemFreq.setFreq( Long.parseLong( value.toString() ) );
 			
 			ItemFreq head = pq.peek();
-			if ( head == null || head.getFreq() < newItemFreq.getFreq() ){
+			if ( pq.size() < topN || head.getFreq() < newItemFreq.getFreq() ){
 				pq.add(newItemFreq);
 			}
 			if (pq.size() > topN){
@@ -88,7 +88,7 @@ public class TopN {
 			newItemFreq.setFreq( sum );
 			
 			ItemFreq head = pq.peek();
-			if ( head == null || head.getFreq() < newItemFreq.getFreq() ){
+			if ( pq.size() < topN || head.getFreq() < newItemFreq.getFreq() ){
 				pq.add(newItemFreq);
 			}
 			if (pq.size() > topN){
