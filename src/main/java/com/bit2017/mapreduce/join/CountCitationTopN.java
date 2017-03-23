d683631187043938430fca971d452215e1e0caa3
HAN <hanys123@gmail.com>
2017-03-23 15:16:43 +0900

    CountTrigram

--- src/main/java/com/bit2017/mapreduce/join/CountCitationTopN.java
+++ src/main/java/com/bit2017/mapreduce/join/CountCitationTopN.java
@@ -15,46 +15,46 @@ import com.bit2017.mapreduce.topn.TopN;
 
 public class CountCitationTopN {
 
-public static void main(String[] args) throws Exception {
-    Configuration conf = new Configuration();
-    Job job = new Job( conf, "Count Trigram" );
-   
-    job.setJarByClass( CountCitationTopN.class ); 
-    
-    job.setOutputKeyClass( Text.class );
-    job.setOutputValueClass( LongWritable.class );
-        
-    job.setMapperClass( CountCitation.MyMapper.class );
-    job.setReducerClass( CountCitation.MyReducer.class );
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+		Job job = new Job(conf, "Count Trigram");
 
-    job.setInputFormatClass( KeyValueTextInputFormat.class );
-    job.setOutputFormatClass( TextOutputFormat.class );
-        
-    FileInputFormat.addInputPath( job, new Path( args[0] ) );
-    FileOutputFormat.setOutputPath( job, new Path( args[1] ) );
-        
-    if ( job.waitForCompletion( true ) == false ) {
-        return;
-    }
+		job.setJarByClass(CountCitationTopN.class);
 
-    Configuration conf2 = new Configuration();
-    Job job2 = new Job(conf2, "Top N");
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(LongWritable.class);
 
-    job2.setJarByClass( TopN.class );
-    job2.setOutputKeyClass(Text.class);
-    job2.setOutputValueClass( LongWritable.class );
+		job.setMapperClass(CountCitation.MyMapper.class);
+		job.setReducerClass(CountCitation.MyReducer.class);
 
-    job2.setMapperClass( TopN.MyMapper.class );
-    job2.setReducerClass( TopN.MyReducer.class );
+		job.setInputFormatClass(KeyValueTextInputFormat.class);
+		job.setOutputFormatClass(TextOutputFormat.class);
 
-    job2.setInputFormatClass(KeyValueTextInputFormat.class);
-    job2.setOutputFormatClass(TextOutputFormat.class);
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
-    // input of Job2 is output of Job
-    FileInputFormat.addInputPath(job2, new Path(args[1]));
-    FileOutputFormat.setOutputPath(job2, new Path( args[1] + "/topN" ));
-    job2.getConfiguration().setInt( "topN", 10 );
+		if (job.waitForCompletion(true) == false) {
+			return;
+		}
 
-    job2.waitForCompletion(true);
- }
+		Configuration conf2 = new Configuration();
+		Job job2 = new Job(conf2, "Top N");
+
+		job2.setJarByClass(TopN.class);
+		job2.setOutputKeyClass(Text.class);
+		job2.setOutputValueClass(LongWritable.class);
+
+		job2.setMapperClass(TopN.MyMapper.class);
+		job2.setReducerClass(TopN.MyReducer.class);
+
+		job2.setInputFormatClass(KeyValueTextInputFormat.class);
+		job2.setOutputFormatClass(TextOutputFormat.class);
+
+		// input of Job2 is output of Job
+		FileInputFormat.addInputPath(job2, new Path(args[1]));
+		FileOutputFormat.setOutputPath(job2, new Path(args[1] + "/topN"));
+		job2.getConfiguration().setInt("topN", 10);
+
+		job2.waitForCompletion(true);
+	}
 }
---
+++ src/main/java/com/bit2017/mapreduce/join/CountTrigram.java
@@ -0,0 +1,112 @@
+package com.bit2017.mapreduce.join;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.KeyValueTextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+import com.bit2017.mapreduce.topn.TopN;
+
+public class CountTrigram {
+
+
+	public static class MyMapper extends Mapper<Text, Text, Text, LongWritable>{
+
+		@Override
+		protected void map(Text key, Text value,
+				Mapper<Text, Text, Text, LongWritable>.Context context)
+				throws IOException, InterruptedException {
+
+			String str = value.toString();
+			String[] word = new String[3];
+			int count = 0;
+			
+			StringTokenizer tokens = new StringTokenizer(str, "\r\n\t,./|()<>{} '\"");
+			while(tokens.hasMoreTokens()){
+				word[count] = tokens.nextToken().toString();
+				if( count < 2 ) {
+					count++;
+				} else if( count == 2 ) {
+			
+					context.write(key, new LongWritable(1L));
+					word[0] = word[1];
+					word[1] = word[2];
+				}
+			}
+
+		}
+
+	}
+	
+	public static class MyReducer extends Reducer<Text, LongWritable, Text, LongWritable>{
+
+		@Override
+		protected void reduce(Text key, Iterable<LongWritable> values,
+				Reducer<Text, LongWritable, Text, LongWritable>.Context context)
+				throws IOException, InterruptedException {
+			long sum = 0;
+
+			for (LongWritable value : values) {
+				sum += value.get();
+			}
+
+			context.write(key, new LongWritable(sum));
+		}
+
+	}
+	
+	public static void main (String args[]) throws Exception{
+		Configuration conf = new Configuration();
+	    Job job = new Job( conf, "Count Trigram" );
+	   
+	    job.setJarByClass( CountTrigram.class ); 
+	    job.setOutputKeyClass( Text.class );
+	    job.setOutputValueClass( LongWritable.class );
+	        
+	    job.setMapperClass( MyMapper.class );
+	    job.setReducerClass( MyReducer.class );
+
+	    job.setInputFormatClass( KeyValueTextInputFormat.class );
+	    job.setOutputFormatClass( TextOutputFormat.class );
+	        
+	    FileInputFormat.addInputPath( job, new Path( args[0] ) );
+	    FileOutputFormat.setOutputPath( job, new Path( args[1] ) );
+	        
+	    if ( job.waitForCompletion( true ) == false ) {
+	        return;
+	    }
+
+	    Configuration conf2 = new Configuration();
+	    Job job2 = new Job(conf2, "Top N");
+
+	    job2.setJarByClass( TopN.class );
+	    job2.setOutputKeyClass(Text.class);
+	    job2.setOutputValueClass( LongWritable.class );
+
+	    job2.setMapperClass( TopN.MyMapper.class );
+	    job2.setReducerClass( TopN.MyReducer.class );
+
+	    job2.setInputFormatClass(KeyValueTextInputFormat.class);
+	    job2.setOutputFormatClass(TextOutputFormat.class);
+
+	    // input of Job2 is output of Job
+	    FileInputFormat.addInputPath(job2, new Path(args[1]));
+	    FileOutputFormat.setOutputPath(job2, new Path( args[1] + "/topN" ));
+	    job2.getConfiguration().setInt( "topN", 10 );
+
+	    job2.waitForCompletion(true);
+
+	}
+	
+
+}
